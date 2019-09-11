c929e82aa9b5f99769d2e55691e77f46a99b3e7a
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2019-09-11 18:48:59 -0400

    L256 Editing with Hooks

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/ToolBar';
 import Grid from '@material-ui/core/Grid';
+import uuid from 'uuid/v4';
 
 function TodoApp() {
     const initialTodos = [
@@ -13,6 +15,19 @@ function TodoApp() {
         {id: 3, task: "Grow Beard", completed: false},
     ];
     const [todos, setTodos] = useState(initialTodos);
+    const addTodo = newTodoText => {
+        setTodos([...todos, {id: uuid(), task: newTodoText, completed: false }]);
+    };
+    const removeTodo = todoId => {
+        const updatedTodos = todos.filter(todo => todo.id !== todoId);
+        setTodos(updatedTodos);
+    };
+    const toggleTodo = todoId => {
+        const updatedTodos = todos.map(todo => 
+                todo.id === todoId ? { ...todo, completed: !todo.completed} : todo
+            );
+        setTodos(updatedTodos);
+    };
     return (
         <Paper 
         style={{
@@ -28,7 +43,12 @@ function TodoApp() {
                  <Typography color='inherit'>TODOS WITH HOOKS</Typography>
              </ToolBar>
          </AppBar>
-         <TodoList todos={todos} />
+         <Grid container justify="center" style={{marginTop: "1rem"}}>
+             <Grid item xs={11} md={8} lg={4}>
+                <TodoForm addTodo={addTodo} />
+                <TodoList todos={todos} removeTodo={removeTodo} toggleTodo={toggleTodo} />
+            </Grid>
+         </Grid>
          </Paper>
     )
 }
