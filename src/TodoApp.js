8f2e2bb2bf45018a8816e973f4e162a5e99c7dd5
yuchuan <yuchuan@yuchuan.com>
2019-09-17 23:43:37 -0400

    L256 Done editing with hooks

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -10,46 +10,52 @@ import uuid from 'uuid/v4';
 
 function TodoApp() {
     const initialTodos = [
-        {id: 1, task: "Clean Fishtank", completed: false},
-        {id: 2, task: "Wash Car", completed: true},
-        {id: 3, task: "Grow Beard", completed: false},
+        { id: 1, task: "Clean Fishtank", completed: false },
+        { id: 2, task: "Wash Car", completed: true },
+        { id: 3, task: "Grow Beard", completed: false },
     ];
     const [todos, setTodos] = useState(initialTodos);
     const addTodo = newTodoText => {
-        setTodos([...todos, {id: uuid(), task: newTodoText, completed: false }]);
+        setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }]);
     };
     const removeTodo = todoId => {
         const updatedTodos = todos.filter(todo => todo.id !== todoId);
         setTodos(updatedTodos);
     };
     const toggleTodo = todoId => {
-        const updatedTodos = todos.map(todo => 
-                todo.id === todoId ? { ...todo, completed: !todo.completed} : todo
-            );
+        const updatedTodos = todos.map(todo =>
+            todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+        );
+        setTodos(updatedTodos);
+    };
+    const editTodo = (todoId, newTask) => {
+        const updatedTodos = todos.map(todo =>
+            todo.id === todoId ? { ...todo, task: newTask } : todo
+        );
         setTodos(updatedTodos);
     };
     return (
-        <Paper 
-        style={{
-            padding: 0,
-            margin: 0,
-            height: "100vh",
-            backgroundColor: "#fafafa"
-        }}
-        elevation={0}
-         >
-         <AppBar color='primary' position='static' style={{ height: "64px" }}>
-             <ToolBar>
-                 <Typography color='inherit'>TODOS WITH HOOKS</Typography>
-             </ToolBar>
-         </AppBar>
-         <Grid container justify="center" style={{marginTop: "1rem"}}>
-             <Grid item xs={11} md={8} lg={4}>
-                <TodoForm addTodo={addTodo} />
-                <TodoList todos={todos} removeTodo={removeTodo} toggleTodo={toggleTodo} />
+        <Paper
+            style={{
+                padding: 0,
+                margin: 0,
+                height: "100vh",
+                backgroundColor: "#fafafa"
+            }}
+            elevation={0}
+        >
+            <AppBar color='primary' position='static' style={{ height: "64px" }}>
+                <ToolBar>
+                    <Typography color='inherit'>TODOS WITH HOOKS</Typography>
+                </ToolBar>
+            </AppBar>
+            <Grid container justify="center" style={{ marginTop: "1rem" }}>
+                <Grid item xs={11} md={8} lg={4}>
+                    <TodoForm addTodo={addTodo} />
+                    <TodoList todos={todos} removeTodo={removeTodo} toggleTodo={toggleTodo} editTodo={editTodo} />
+                </Grid>
             </Grid>
-         </Grid>
-         </Paper>
+        </Paper>
     )
 }
 export default TodoApp;
--- src/TodoList.js
+++ src/TodoList.js
@@ -5,16 +5,16 @@ import Divider from '@material-ui/core/Divider';
 import Todo from './Todo';
 
 
-function TodoList({todos, removeTodo, toggleTodo}) {
+function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
     return (
         <Paper>
             <List>
-            {todos.map(todo => (
-                <>
-                    <Todo id={todo.id} task={todo.task} key={todo.id} completed={todo.completed} removeTodo={removeTodo} toggleTodo={toggleTodo} />
-                <Divider />
-                </>
-            ))}
+                {todos.map(todo => (
+                    <>
+                        <Todo id={todo.id} task={todo.task} key={todo.id} completed={todo.completed} removeTodo={removeTodo} toggleTodo={toggleTodo} editTodo={editTodo} />
+                        <Divider />
+                    </>
+                ))}
             </List>
         </Paper>
     )
